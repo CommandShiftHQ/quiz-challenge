a14073acd638560014cc8780658df166e55a97dd
Michael Harrison <harrim91@hotmail.co.uk>
2021-11-11 21:04:46 +0000

    Add prompting

--- index.js
+++ index.js
@@ -1,4 +1,50 @@
+const promptSync = require('prompt-sync');
 const Question = require('./src/question');
+
 const question1 = new Question('Are mince pies better than fig rolls?', 'yes');
-question1.verify('no')
-console.log(question1.verify('no'));
+const question2 = new Question('What is the capital of France?', 'Paris');
+const question3 = new Question('What is the capital of Germany?', 'yes');
+const question4 = new Question('What is the capital of Djibouti?', 'Djibouti');
+const question5 = new Question('What is the capital of Peru?', 'Lima');
+
+const prompt = promptSync();
+
+let guess = prompt(question1.question);
+
+let result = question1.verify(guess);
+
+console.log(result);
+
+console.log('---');
+
+guess = prompt(question2.question);
+
+result = question2.verify(guess);
+
+console.log(result);
+
+console.log('---');
+
+guess = prompt(question3.question);
+
+result = question3.verify(guess);
+
+console.log(result);
+
+console.log('---');
+
+guess = prompt(question4.question);
+
+result = question4.verify(guess);
+
+console.log(result);
+
+console.log('---');
+
+guess = prompt(question5.question);
+
+result = question5.verify(guess);
+
+console.log(result);
+
+console.log('---');
